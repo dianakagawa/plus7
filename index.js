2c25bd252f4c48cddb73a960bf3f4b4f4eb746d6
dianakagawa <drcnakagawa@gmail.com>
2024-02-05 21:42:29 -0300

    Add displayForecast function and new forecast api

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // Formate date
 function formatDate(date) {
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-
   let days = [
     "Sunday",
     "Monday",
@@ -13,6 +10,8 @@ function formatDate(date) {
     "Saturday",
   ];
 
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
@@ -39,11 +38,42 @@ function displayTemperature(response) {
   currentDateElement.innerHTML = formatDate(date);
 }
 
+// display forecast details
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#daily-weather");
+  forecastElement.innerHTML = null;
+  let forecast = response.data.daily;
+
+  let dayShorten = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      const dayOfWeek = dayShorten[index];
+      forecastElement.innerHTML += `
+      <div class="day-forecast">
+        <h3>${dayOfWeek}</h3>
+        <img src="${forecastDay.condition.icon_url}" alt="${
+        forecastDay.condition.icon
+      }" />
+        <div class="weather-forecast-temperature">
+          <strong>${Math.round(
+            forecastDay.temperature.maximum
+          )}°</strong> ${Math.round(forecastDay.temperature.minimum)}°
+        </div>
+      </div>`;
+    }
+  });
+}
+
 // search by city
 function search(city) {
   let apiKey = "2efd3o2bd61faa9fa0ta27735b061c64";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
+
+  let apiCurrent = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiCurrent).then(displayTemperature);
+
+  let apiForecast = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios.get(apiForecast).then(displayForecast);
 }
 
 // Event handler
@@ -57,4 +87,4 @@ let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
 // Default city
-search("Asuncion");
+search("Asunción");
